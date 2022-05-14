a617a70c77a8c9841c386ec81d66484ad8943e8f
Zeloxey <TheoTailame@gmail.com>
2022-05-14 12:25:01 +0200

    Update

--- testtt.js
+++ testtt.js
@@ -2,6 +2,11 @@ import * as R from 'ramda';
 
 import fs from "fs";
 
+import chiSquaredTest from 'chi-squared-test';
+
+import PCA from 'pca-js'
+
+
 import {csvToJson, getCSV, getCSV2, csvToArray} from "./fct.js";
 import {csvToJSON} from "./csvToJSON.js";
 
@@ -9,97 +14,123 @@ const csv = fs.readFileSync('./CSV_file/Iris.csv', 'utf8')
 const columns = ['Id','SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm','Species']
 
 
+const data = csvToJSON(csv)
+console.log(data);
+// console.log((R.pluck('Id', dodo)));
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const dodo = csvToJSON(csv)
-console.log(dodo);
-
-console.log((R.pluck('Id', dodo)));
-
-import chiSquaredTest from 'chi-squared-test';
-
-// We expect a fair die
-var expected = [2, 2, 2, 2, 2, 2];
-
-// Looks pretty unfair...
-var observed = [6, 3, 3, 0, 0, 0];
-
-// Reduction in degrees of freedom is 1, since knowing 5 categories determines the 6th
-var reduction = 1;
-
-var probability = chiSquaredTest(observed, expected, reduction);
-// Gives 0.010362, which indicates that it's unlikely the die is fair 
-console.log(probability)
-// However, something a little more likely
-observed = [1, 2, 4, 4, 2, 1];
-probability = chiSquaredTest(observed, expected, reduction);
-console.log(probability.probability)
-// Gives back 0.415881, which is indicates that they did come from the same distribution (by most statistical standards)
-
-
-
-
-
-const getpercentage =  R.divide(R.__,100);
-
-const getIndexSplit = R.pipe(
-    R.converge(R.multiply(getpercentage(80)), [R.length]),
+const splitIndex = R.pipe(
+    R.converge(R.multiply(0.80), [R.length]),
     Math.round,
 );
 
-
-const splitInTrainingAndTesting = R.converge(R.splitAt,[getIndexSplit,R.identity]);
-
-const getTraining = R.pipe(splitInTrainingAndTesting,R.head);
-const getTesting = R.pipe(splitInTrainingAndTesting,R.last);
+const splitData = R.splitAt(splitIndex(data), data);
 
 
-console.log(getTraining(dodo));
-console.log(getTesting(dodo));
+const trainingData = splitData[0];
+const testingData = splitData[1];
 
 
+console.log(trainingData);
+console.log(testingData);
 
 
-const getObservedValues = R.pipe(
+const getObserved = R.pipe(
     R.pluck('Species'),
     R.countBy(R.identity),
-    R.values
+    R.values,
 );
 
-const getNumberOfOutput = R.pipe(
-    getObservedValues,
-    R.length
+const getExpected = R.pipe(
+    getObserved,
+    R.converge(
+    R.repeat,
+    [R.converge(R.divide,[R.sum,R.length]),R.length],
+    ),
 );
 
-console.log(getObservedValues(dodo));
-
-const getExpectedValues = R.converge(
-    R.repeat,[
-        R.converge(R.multiply,[
-        R.converge(R.divide(1),[getNumberOfOutput]),
-        R.length
-        ]),
-        getNumberOfOutput
-    ]
+const getDegreeOfFreedom = R.pipe(
+    getObserved,
+    R.converge(R.subtract(1), [R.length]),
+    Math.abs,
 );
 
-console.log(getExpectedValues(dodo));
 
-const probabilities = chiSquaredTest(getObservedValues(dodo), getExpectedValues(dodo), reduction);
-console.log(probabilities)
+
+const observed = getObserved(testingData);
+const expected = getExpected(testingData);
+const DegreeOfFreedom = getDegreeOfFreedom(testingData)
+
+
+const probabilities = chiSquaredTest(observed, expected, DegreeOfFreedom);
+console.log(probabilities);
+
+
+const getValues = R.pipe(
+    
+    R.map(
+        R.pipe(
+            R.values,
+            R.drop(2),
+            R.dropLast(1),
+        )
+    )
+        
+    
+);
+
+const values = getValues(trainingData);
+
+console.log(values);
+
+
+const vectors = PCA.getEigenVectors(values);
+
+var first = PCA.computePercentageExplained(vectors,vectors[0])
+
+var topTwo = PCA.computePercentageExplained(vectors,vectors[0],vectors[1])
+
+
+// var adData = PCA.computeAdjustedData(trainingData,vectors[0])
+
+// var data = [[40,50,60],[50,70,60],[80,70,90],[50,60,80]];
+// var vectors = PCA.getEigenVectors(data);
+// var first = PCA.computePercentageExplained(vectors,vectors[0])
+// // 0.8434042149581044
+// var topTwo = PCA.computePercentageExplained(vectors,vectors[0],vectors[1])
+// // 0.9700602484397556
+
+
+// var adData = PCA.computeAdjustedData(data,vectors[0])
+
+// const getValues = R.pipe(
+//     R.map(
+//         R.pipe(
+//             R.values,
+//             R.drop(1),
+//             R.dropLast(1),
+//         )
+//     )
+// );
+
+// const getVectors = R.pipe(
+//     getValues,
+//     PCA.getEigenVectors
+// );
+
+// //console.log(getVectors(dataset));
+
+// const firstsd = R.pipe(
+//     getVectors,
+//     R.converge(PCA.computePercentageExplained, [R.identity, R.prop(0)]),
+// );
+
+// const topTwosd = R.pipe(
+//     getVectors,
+//     R.converge(PCA.computePercentageExplained, [R.identity, R.prop(0), R.prop(1)]),
+// );
+
+// console.log(firstsd(dodo));
+// console.log(topTwosd(dodo));
